9f8edfa37e9d24412714b40fde10a3828a71950b
Nick Lorch <nlorch@gmail.com>
2019-01-27 14:12:39 -0600

    Updates

---
+++ precache-manifest.640ac2301cc079d604d555e7c82939fd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3c504903c907af551e3",
+    "url": "/wordlist/static/js/runtime~main.b3c50490.js"
+  },
+  {
+    "revision": "fc3523f6c59bd1b0dce9",
+    "url": "/wordlist/static/js/main.fc3523f6.chunk.js"
+  },
+  {
+    "revision": "4d91f67a58526f5fe88d",
+    "url": "/wordlist/static/js/1.4d91f67a.chunk.js"
+  },
+  {
+    "revision": "fc3523f6c59bd1b0dce9",
+    "url": "/wordlist/static/css/main.bfec3ee2.chunk.css"
+  },
+  {
+    "revision": "4d91f67a58526f5fe88d",
+    "url": "/wordlist/static/css/1.f69d9e24.chunk.css"
+  },
+  {
+    "revision": "7d1246231c5f1dbf079d4acf4ce4aca9",
+    "url": "/wordlist/index.html"
+  }
+];
