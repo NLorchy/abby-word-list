42576641a9c453bd6064838073786d1487fd4a69
Nick Lorch <nlorch@gmail.com>
2020-05-22 09:52:35 -0500

    Updates

--- precache-manifest.f8e0d86ca89055f998390aaead0b718d.js
+++ precache-manifest.f8e0d86ca89055f998390aaead0b718d.js
@@ -4,15 +4,15 @@ self.__precacheManifest = [
     "url": "/wordlist/static/js/runtime~main.b3c50490.js"
   },
   {
-    "revision": "fc3523f6c59bd1b0dce9",
-    "url": "/wordlist/static/js/main.fc3523f6.chunk.js"
+    "revision": "4861e96f2fff66370e7a",
+    "url": "/wordlist/static/js/main.4861e96f.chunk.js"
   },
   {
     "revision": "4d91f67a58526f5fe88d",
     "url": "/wordlist/static/js/1.4d91f67a.chunk.js"
   },
   {
-    "revision": "fc3523f6c59bd1b0dce9",
+    "revision": "4861e96f2fff66370e7a",
     "url": "/wordlist/static/css/main.bfec3ee2.chunk.css"
   },
   {
@@ -20,7 +20,7 @@ self.__precacheManifest = [
     "url": "/wordlist/static/css/1.f69d9e24.chunk.css"
   },
   {
-    "revision": "7d1246231c5f1dbf079d4acf4ce4aca9",
+    "revision": "f9b6c779ecdf66630ce83e694a084905",
     "url": "/wordlist/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/wordlist/precache-manifest.640ac2301cc079d604d555e7c82939fd.js"
+  "/wordlist/precache-manifest.f8e0d86ca89055f998390aaead0b718d.js"
 );
 
 workbox.clientsClaim();
